7a0ad13a6dc722cee5f4e965ca80af6cd6d79fcd
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2025-07-26 09:49:38 +0700

    import asset

---
+++ Assets/Demo/Utils/SingletonBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    T t = FindFirstObjectByType<T>();
+                    if (t != null)
+                    {
+                        _instance = t;
+                    }
+                    else
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
